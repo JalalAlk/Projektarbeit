f249b0aaac8257e8f36fc48204b95b2ff0fe11d8
Omran Alkurdi <omranal791@gmail.com>
2024-06-12 00:04:00 +0200

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,54 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import astroPlugin from 'eslint-plugin-astro';
+import astroParser from 'astro-eslint-parser';
 
 export default [
+  // Globale Einstellungen
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+
+  // Astro-spezifische Einstellungen
   {
+    files: ["*.astro"],
+    plugins: {
+      astro: astroPlugin,
+    },
+    env: {
+      node: true,
+      "astro/astro": true,
+      es2020: true,
+    },
+    parser: astroParser,
+    parserOptions: {
+      parser: "@typescript-eslint/parser",
+      extraFileExtensions: [".astro"],
+      sourceType: "module",
+    },
+    rules: {
+      "astro/no-conflict-set-directives": "error",
+      "astro/no-unused-define-vars-in-style": "error",
+    },
+  },
+
+  // Einstellungen für JavaScript in .astro-Dateien
+  {
+    files: ["**/*.astro/*.js", "*.astro/*.js"],
+    env: {
+      browser: true,
+      es2020: true,
+    },
+    parserOptions: {
+      sourceType: "module",
+    },
+    rules: {
+      "prettier/prettier": "off",
+    },
+  },
+
+  // Standard-Regeln
+  {
+    files: ["*.js"],
     rules: {
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 // Fehlerhaftes JavaScript-Codebeispiel
 function add(a, b) {
-     a + b
-  }
-  
-  const result = add(5, 10);
-  console.log("Das Ergebnis ist: " + result):
-  
+  a + b;
+}
+
+const result = add(5, 10);
+console.log('Das Ergebnis ist: ' + result);
+length;
+
