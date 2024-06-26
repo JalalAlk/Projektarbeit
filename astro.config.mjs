797152128f05b58e2963a2e6254612049aeeef78
Omran Alkurdi <omranal791@gmail.com>
2024-06-26 15:32:20 +0200

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 //test
 export default defineConfig({
-  site: 'https://Projektarbeit.github.io',
-  base: '/REPOSITORY_NAME/', // Ersetze 'REPOSITORY_NAME' mit dem Namen deines Repositories
-  build: {
-    outDir: 'dist', // Standard-Build-Verzeichnis ist 'dist'
-  },
-});
+  site: 'https://projektarbeit.github.io/',
 
+});
