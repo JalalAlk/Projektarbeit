9e0100e40df71192df1768db14e812560740a456
Jalal <jalalalkhatba1999@gmail.com>
2024-09-09 17:02:21 +0200

    qa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 //test
 export default defineConfig({
-  //site: 'https://projektarbeit.github.io/',
-  base: '/Projektarbeit/',
-
+  site: 'https://projektarbeit.github.io/',
+  base: '/',
 });
